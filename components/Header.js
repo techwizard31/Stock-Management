a9a5815187ce662702ed38bf37b7abc8ea496320
techwizard31 <istaprasad.patra@gmail.com>
2025-04-14 22:43:45 +0530

    fixing

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ function Header() {
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-3 text-xl">Stock Management System</span>
+          <span className="ml-3 text-xl">Inventory Management System</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <a className="mr-5 hover:text-gray-900">First Link</a>
